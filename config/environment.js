2e3ee2df64a8b30b9c487dca237ec2b26f1c077b
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-03-19 00:10:32 +0530

    add phoenix as defult-src

--- config/environment.js
+++ config/environment.js
@@ -31,30 +31,17 @@ module.exports = function(environment) {
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
     ENV.contentSecurityPolicy = {
   // Deny everything by default
-  'default-src': "'none'",
-  
-  // Allow scripts at https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js
-  'script-src': ["'self'","http://localhost:4000"],
-  
-  // Allow fonts to be loaded from http://fonts.gstatic.com
+  'default-src': ['http://localhost:4000'],
+  'script-src': ["'self'",'http://localhost:4000'],
   'font-src': ["'self'"],
-  
-  // Allow data (xhr/websocket) from api.mixpanel.com and custom-api.local
   'connect-src': ["'self'", "http://localhost:4000"],
-  
-  // Allow images from the origin itself (i.e. current domain)
   'img-src': "'self'",
-  
-  // Allow CSS loaded from https://fonts.googleapis.com
   'style-src': ["'self'", "https://fonts.googleapis.com"],
-  
-  // Omit `media-src` from policy
-  // Browser will fallback to default-src for media resources (which is 'none', see above)
   'media-src': null
-  };
-  }
+};
+}
 
-  if (environment === 'test') {
+if (environment === 'test') {
     // Testem prefers this...
     ENV.locationType = 'none';
 
